178116ee5df2e45c6daab3affb7854990868d418
HirokiOtani <hf3311@gmail.com>
2021-02-01 17:44:01 +0900

    modify little

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+const knex = require("./knex");
+
 app.use(express.json());
 
+const port = 4000;
+
 app.get("/api/apples", async (_, res) => {
-  res.send("OK");
-  res.status(200);
+  const data = await knex.select().from("apples");
+  res.status(200).send(data);
 });
 
-const port = 4000;
+app.post("/api/apples", async (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+//post
+//patch/put
+//delete
