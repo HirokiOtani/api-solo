13a2d416a10a54f8d6d363484f741667a9949a4a
HirokiOtani <hf3311@gmail.com>
2021-02-01 22:55:35 +0900

    finish CRUD

--- server.js
+++ server.js
@@ -1,27 +1,59 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
 const knex = require("./knex");
+const path = require("path");
 
+//app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const port = 4000;
 
-app.get("/api/apples", async (_, res) => {
-  const data = await knex.select().from("apples");
-  res.status(200).send(data);
+app.get("/oranges", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.post("/api/apples", async (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+app.post("/oranges/post", async (req, res) => {
+  await knex("oranges").insert({
+    name: req.body.name,
+    taste: req.body.taste,
+    origin: req.body.origin,
+    season: req.body.season,
+  });
+  res
+    .status(200)
+    .send(
+      "Add a new orange! :" +
+        req.body.name +
+        req.body.taste +
+        req.body.origin +
+        req.body.season
+    );
+});
+
+app.post("/oranges/patch", async (req, res) => {
+  await knex("oranges").where({ name: req.body.name }).update({
+    taste: req.body.taste,
+    origin: req.body.origin,
+    season: req.body.season,
+  });
+  res.status(200).send("Updated!");
+});
+
+app.post("/oranges/delete", async (req, res) => {
+  await knex("oranges").where({ name: req.body.name }).del();
+  res.status(200).send("Deleted!");
+});
+
+app.get("/oranges/get", async (_, res) => {
+  const data = await knex.select().from("oranges");
+  res.status(200).send(`<h2>${JSON.stringify(data)}</h2>`);
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-//post
-//patch/put
-//delete
